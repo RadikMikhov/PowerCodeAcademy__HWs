ed4ae5a4da42a3ad63f5df622927e7dd16e48242
Radik <radikmikhov@gmail.com>
2021-05-29 16:49:21 +0300

    Some change to hw18

--- hw18/js/script.js
+++ hw18/js/script.js
@@ -1,19 +1,23 @@
-let add = document.querySelector(".add__btn");
+let inputText = document.querySelector(".todo__input");
+let btn = document.querySelector(".add__btn");
+let newLi = document.querySelector(".todo__item");
 
-add.addEventListener("click", function () {
-  let inputForm = document.forms.formInput;
-  let textValue = inputForm.elements.text.value;
-  if (textValue !== '') {
-    addTodo(textValue);
+btn.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    submit.click();
   }
-  textValue.value = "";
-  
 });
 
-function addTodo(x) {
-  let parentLeft = document.querySelector(".todo__left");
-  let child = document.createElement("li");
-  child.classList.add("todo__item");
-  child.innerHTML = x;
-    parentLeft.prepend(child);
+function addItem() {
+  let getValue = inputText.value;
+  let createElement = document.createElement("li");
+  createElement.classList.add("todo__item");
+
+  createElement.textContent = getValue;
+  newLi.appendChild(createElement);
+  inputText.value = "";
 }
+
+btn.addEventListener('click', addItem);
+
