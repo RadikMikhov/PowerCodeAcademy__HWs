1cb4718a8cb51bdc2a6342ac0c15b4d9ce3990d6
Radik <radikmikhov@gmail.com>
2021-05-30 23:53:42 +0300

    some change to hw18

--- hw18/js/script.js
+++ hw18/js/script.js
@@ -1,7 +1,10 @@
 let inputText = document.querySelector(".todo__input");
 let btn = document.querySelector(".add__btn");
-let newLi = document.querySelector(".todo__item");
+let newLi = document.querySelector(".todo__left");
+let newCheckbox = document.querySelector(".checkbox");
+let compleatedItem = document.querySelector(".comleated__item");
 
+btn.addEventListener('click', addItem);
 btn.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
@@ -14,10 +17,13 @@ function addItem() {
   let createElement = document.createElement("li");
   createElement.classList.add("todo__item");
 
+  let createCheckbox = document.createElement('input');
+  createCheckbox.setAttribute('class', 'checkbox');
+  createCheckbox.setAttribute('type', 'checkbox');
+  newLi.appendChild(createCheckbox);
+  console.log(createCheckbox)
+
   createElement.textContent = getValue;
   newLi.appendChild(createElement);
   inputText.value = "";
-}
-
-btn.addEventListener('click', addItem);
-
+};
